b9fde2ee38e20d3e40a9a01c091dac6c887410e3
İsmail Akbudak <ismail.akbudak@lab2023.com>
2018-08-10 12:59:47 +0300

    Implement account resource management end point

---
+++ lib/smartcat_sdk/rest/account.rb
@@ -0,0 +1,20 @@
+require 'smartcat_sdk/rest/base_request'
+
+module SmartcatSDK
+  module REST
+    class Account < BaseRequest
+      def initialize(*args)
+        super(*args)
+        @resource = 'account'
+      end
+
+      def detail
+        prepare_request('get', "#{@resource}")
+      end
+
+      def mtengines
+        prepare_request('get', "#{@resource}/mtengines")
+      end
+    end
+  end
+end
